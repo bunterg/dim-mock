6d32bed67609512f66b2923ed1f66f962a30fd63
Bernardo Garcia <bmgg358@gmail.com>
2019-04-18 15:44:04 -0400

    columntype object

--- index.js
+++ index.js
@@ -1,59 +1,95 @@
-var faker = require('faker');
+const faker = require('faker');
+const fs = require('fs');
 
-var randomName = faker.name.findName(); // Rowan Nikolaus
-var randomEmail = faker.internet.email(); // Kassandra.Haley@erich.biz
-var randomCard = faker.helpers.createCard(); 
-
-
-class DmRepo {
-    constructor(fakerString, schema) {
-        this.touples = [];
-        this.pattern = fakerString;
-        this.schema = schema
-    }
-
-    append() {
-        const dataLength = this.touples.length;
-        const car = this.fakeTouple(dataLength);
-        this.touples.push(car);
-    }
-
-    fakeTouple() {
-        const i = this.touples.length
-        return [i, ...faker.fake(this.pattern).split('//')];   
-    }
-
-    get data() {
-        return this.touples;
-    }
-
-    mock() {
-        return this.touples.map( v => {
-            return new DmMock(v, this.schema);
-        })
-    }
-
-    get mockData() {
-        return this.mock(this.schema)
+function makeColumnType (columnName, patternFunction) {
+    return {
+        name: columnName,
+        patternFunction: patternFunction
     }
 }
 
-class DmCarsRepo extends DmRepo {
-    constructor(quantity) {
-        super("{{company.companyName}}//{{hacker.noun}}//{{date.recent}}", ["make_id", "make_name", "model_name", "record_dt"]);
-        for (let i = 0; i < quantity; i++) {
-            this.append()
-        }
+class DmRepo {
+    constructor(columTypes) {
+        this.tuples = [];
+        this.struct = columTypes;
+        this.count = 0;
+    }
+
+    append() {
+        const id = this.tuples.length;
+        const tuple = this.createTuple(id);
+        this.tuples.push(tuple);
+    }
+
+    createTuple() {
+        this.count++
+        return new DmMock(this.struct, this.count)
+    }
+
+    get schema() {
+        return this.struct.map(obj => obj.name)
+    }
+
+    get data() {
+        return this.tuples;
+    }
+
+    get csv() {
+        return this.tuples.map(t => t.toArray())
     }
 }
 
 class DmMock {
-    constructor(data, schema) {
-        schema.forEach((columnName, i) => { 
-            this[columnName] = data[i]
+    constructor(schema, id) {
+        schema.forEach((dataType) => {
+            this[dataType.name] = dataType.patternFunction(id)
         })
+    }
+    
+    toArray() {
+        return Object.getOwnPropertyNames(this).reduce((a, e) => {
+            if(typeof(a) == 'string') a=[this[a]]
+            a.push(this[e])
+            return a
+        })
+    }
+
+    toCSV() {
+        return `"${this.toArray().join('","')}"`;
     }
 }
 
-const dmCarsRepo = new DmCarsRepo(1000);
-console.table(dmCarsRepo.mockData);
+class DmShuffleMock {
+    constructor(schema) {
+        this.struct = schema;
+        this.count = 0;
+    }
+    toArray() {
+        return this.struct.map(columnType => columnType.patternFunction(this.count));
+    }
+
+    get schema() {
+        return this.struct.map(obj => obj.name)
+    }
+
+    toCSV() {
+        return `"${this.toArray().join('","')}"`;
+    }
+}
+
+
+const dmCarsRepo = new DmRepo([
+    makeColumnType("make_id", (i) => i),
+    makeColumnType("make_name", () => faker.fake("{{company.companyName}}")),
+    makeColumnType("model_name", () => faker.fake("{{random.word}}")),
+    makeColumnType("record_dt", () => new Date(faker.fake("{{date.recent}}")))
+]);
+
+const writeble = fs.createWriteStream('mycsv.csv');
+writeble.write(`"${dmCarsRepo.schema.join('","')}"`);
+writeble.write('\n');
+for (let i = 0; i < 1000000; i++) {
+    writeble.write(dmCarsRepo.createTuple().toCSV());
+    writeble.write('\n');
+}
+writeble.close();
